149a5f290930d85c01ec0ee9de27d88ba87da1fd
frank@epigmacpro <zhangc518@gmail.com>
2017-10-26 09:54:58 -0700

    adding ref genome

--- makeWashU.sh
+++ makeWashU.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
-base_url="http://enhancer.sdsc.edu/sochee/ENCODE/ipstar/"
-samples=(`curl -s $base_url| grep -o "[a-zA-Z0-9_]*.bw" | uniq`)
+source_url="http://enhancer.sdsc.edu/sochee/ENCODE/ipstar/"
+genome="hg19"
+
+samples=(`curl -s $source_url | grep -o "[a-zA-Z0-9_]*.bw" | uniq`)
 
 out_json="tracks.json"
-touch out_json
+touch $out_json
 echo "[" >$out_json
 
-touch $out_json
+# adding tracks 
 for f in samples
 do
     s=${f%.*}
-    l=$base_url$f
+    l=$source_url$f
     sed -e "s@\$url@$l@g;s@\$name@$s@g" base.json |tee -a $out_json
 done
 
 
-    
+# adding genome 
+ref_genome="refGene"
+sed -e "s@\$genome@$ref_genome@g" base_genome.json |tee -a $out_json
+echo "]" >> $out_json
+
+# finished
+echo "traks.json generated"
 
 
 
-
-
+#http://egg.wustl.edu/d/hg19/refGene.gz
+#http://egg.wustl.edu/d/hg38/refGene.gz
+#http://egg.wustl.edu/d/hg38/gencodeV23.gz
+#http://egg.wustl.edu/d/mm10/refGene.gz
