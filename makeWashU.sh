ead7561ef5d5c533fe81913e789087b077d0964b
frank@epigmacpro <zhangc518@gmail.com>
2017-10-26 15:51:42 -0700

    run succesfully

--- makeWashU.sh
+++ makeWashU.sh
@@ -6,10 +6,10 @@ samples=(`curl -s $source_url | grep -o "[a-zA-Z0-9_]*.bw" | uniq`)
 
 out_json="tracks.json"
 touch $out_json
-echo "[" >$out_json
+printf "[" >$out_json
 
 # adding tracks 
-for f in samples
+for f in ${samples[@]}
 do
     s=${f%.*}
     l=$source_url$f
@@ -20,10 +20,13 @@ done
 # adding genome 
 ref_genome="refGene"
 sed -e "s@\$genome@$ref_genome@g" base_genome.json |tee -a $out_json
-echo "]" >> $out_json
+printf "]" >> $out_json
 
 # finished
 echo "traks.json generated"
+
+scp ./tracks.json zhc268@epigenomics.sdsc.edu:/home/zhc268/setQC_reports
+#http://epigenomegateway.wustl.edu/browser/?genome=hg19&tknamewidth=275&datahub=http://epigenomics.sdsc.edu:8084/tracks.json
 
 
 
