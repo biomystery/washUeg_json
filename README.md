f3f87e07847f82976ef5da7bbf0b3177ed74e041
frank@tscc <zhangc518@gmail.com>
2018-03-28 16:46:24 -0700

    make it more general for all users

---
+++ README.md
@@ -0,0 +1,8 @@
+# a tool to generate bw track json for washU epig browser 
+
+## Install 
+
+``` shell
+export PATH=$PATH:$(pwd)
+chmod u+x makeWashU.sh base.json base_genome.json
+```
--- makeWashU.sh
+++ makeWashU.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-source_url="http://enhancer.sdsc.edu/sochee/ENCODE/ipstar/"
-genome="hg19"
 
-samples=(`curl -s $source_url | grep -o "[a-zA-Z0-9_]*.bw" | uniq`)
+if [ $# -lt 3 ];then
+    echo "Need  parameters! <baseurl for json file> <genome><bigwig file list(.txt)>"
+    exit
+fi
+
+source_url=$1 
+genome=$2 
+bws=$3
+
+samples=($(cat $bws))
+
 
 out_json="tracks.json"
 touch $out_json
@@ -13,24 +21,18 @@ for f in ${samples[@]}
 do
     s=${f%.*}
     l=$source_url$f
-    sed -e "s@\$url@$l@g;s@\$name@$s@g" base.json |tee -a $out_json
+    sed -e "s@\$url@$l@g;s@\$name@$s@g" $(which base.json) |tee -a $out_json
 done
 
 
 # adding genome 
 ref_genome="refGene"
-sed -e "s@\$genome@$ref_genome@g" base_genome.json |tee -a $out_json
+sed -e "s@\$genome@$ref_genome@g" $(which base_genome.json) |tee -a $out_json
 printf "]" >> $out_json
 
 # finished
 echo "traks.json generated"
 
-scp ./tracks.json zhc268@epigenomics.sdsc.edu:/home/zhc268/setQC_reports
 #http://epigenomegateway.wustl.edu/browser/?genome=hg19&tknamewidth=275&datahub=http://epigenomics.sdsc.edu:8084/tracks.json
 
 
-
-#http://egg.wustl.edu/d/hg19/refGene.gz
-#http://egg.wustl.edu/d/hg38/refGene.gz
-#http://egg.wustl.edu/d/hg38/gencodeV23.gz
-#http://egg.wustl.edu/d/mm10/refGene.gz
